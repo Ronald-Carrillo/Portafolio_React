16c9083ac7c6c18c8499e096210527e243e8f2a3
Ronald Carrillo <rcarrilloparra2016@gmail.com>
2022-07-08 22:36:02 -0500

    setChange scroll

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -1,11 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import logo from "../image/logo.png"
 import "../style/Header.css"
 
 const Header = () => {
+    const [activeLink, setactiveLink] = useState("home")
+    const [scolled, setScolled] = useState(false)
     const [change, setChange] = useState(true)
     console.log('change', change)
     
+    useEffect(() => {
+     const onScroll = () => {
+        if(window.scrollY > 50 ){
+            setScolled(true)
+        } else{
+            setScolled(false)
+        }
+     } 
+
+      window.addEventListener("scroll", onScroll)    
+      return () => window.removeEventListener("scroll", onScroll) 
+    }, [])
+    
+    
+    const onUpdateActiveLink = (value) =>{
+         setactiveLink(value)
+    }
+
   return (
     <div>
             {change === true &&
@@ -18,43 +38,43 @@ const Header = () => {
                     </div>
                     <div className='nav'>
                         <div className='home'>
-                            <a href="`#${id.seccion}`">Home</a>
                         </div>
+                            <a href="#home" className={activeLink === "home" ? "active navbar-link": "navbar-link"} onClick={()=>onUpdateActiveLink("home")}>Home</a>
                         <div className='aboutMe'>
-                            <a href='#about_me'>Sobre mi </a>
+                            <a href='#about_me' className={activeLink === "about_me" ? "active navbar-link": "navbar-link"}  onClick={()=>onUpdateActiveLink("about_me")}>Sobre mi </a>
                         </div>
                         <div className='portfolio'>
-                            <a href='#portafolio'>Portafolio</a>
+                            <a href='#portafolio' className={activeLink === "portafolio" ? "active navbar-link": "navbar-link"}  onClick={()=>onUpdateActiveLink("portafolio")}>Portafolio</a>
                         </div>
                         <div className='contact'>
-                            <a href='#cantacte_me '>Contactame</a>
+                            <a href='#cantacte_me' className={activeLink === "cantacte_me" ? "active navbar-link": "navbar-link"}  onClick={()=>onUpdateActiveLink("cantacte_me")}>Contactame</a>
                         </div>
                     </div>
             </header>
             }
              { change === false &&
-                <header className='headerResponsive'>
-                    <div className='headerImageResponsive'>
-                        <img src={logo} alt=""/>
+                       <header className='header'>
+                <div className='headerImage'>
+                    <img src={logo} alt=""/>
+                </div>
+                    <div className='hamburger'>
+                        <i onClick={()=>setChange(false)}  className="ri-menu-line"></i>
                     </div>
-                    <div className='close'>
-                        <i onClick={()=>setChange(true)}  class="ri-close-line"></i>
-                    </div>
-                        <div className='navResponsive'>
-                            <div className='home'>
-                                <a href='#home'>Home</a>
-                            </div>
-                            <div className='aboutMe'>
-                                <a href='#about_me'>Sobre mi </a>
-                            </div>
-                            <div className='portfolio'>
-                                <a href='#portafolio'>Portafolio</a>
-                            </div>
-                            <div className='contact'>
-                                <a href='#cantacte_me '>Contactame</a>
-                            </div>
+                    <div className='nav'>
+                        <div className='home'>
                         </div>
-                </header>
+                            <a href="#home" className={activeLink === "home" ? "active navbar-link": "navbar-link"} onClick={()=>onUpdateActiveLink("home")}>Home</a>
+                        <div className='aboutMe'>
+                            <a href='#about_me' className={activeLink === "about_me" ? "active navbar-link": "navbar-link"}  onClick={()=>onUpdateActiveLink("about_me")}>Sobre mi </a>
+                        </div>
+                        <div className='portfolio'>
+                            <a href='#portafolio' className={activeLink === "portafolio" ? "active navbar-link": "navbar-link"}  onClick={()=>onUpdateActiveLink("portafolio")}>Portafolio</a>
+                        </div>
+                        <div className='contact'>
+                            <a href='#cantacte_me' className={activeLink === "cantacte_me" ? "active navbar-link": "navbar-link"}  onClick={()=>onUpdateActiveLink("cantacte_me")}>Contactame</a>
+                        </div>
+                    </div>
+            </header>
             }
     </div>
   )
